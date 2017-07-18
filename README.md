e7458386785c4fded33e27ba9f0824a18b487ca5
Mark Jordan <mjordan@sfu.ca>
2017-07-18 16:20:21 -0700

    Second commit.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Script to generate Bags for objects in a remote Islandora instance using Islandora's REST interface.
 
-Still a work in progress. Doesn't create Bags yet but does everything else.
+Still a work in progress.
 
 ## Requirements
 
 * On the target Islandora instance
-  * [Islandora REST](https://github.com/discoverygarden/islandora_rest)
+  * [Islandora REST](https://github.com/discoverygarden/islandora_rest). The anonymous user should have Islandora REST's 'View objects' permission.
 * On the system where the script is run
   * PHP 5.5.0 or higher.
   * [Composer](https://getcomposer.org)
@@ -22,11 +22,18 @@ Still a work in progress. Doesn't create Bags yet but does everything else.
 
 This script:
 
-1. Scrapes the URLs for all objects in a collection from the collection browse pages
-1. For each object,
-  1. Queries Islandora's REST interface to get a list of its datastreams
-  1. Fetches each datastream's content file
-  1. Generates a Bag containing all of the object's content files.
+1. Queries Solr for the URLs for all objects in the specified namespace, and for each object:
+1. Queries Islandora's REST interface to get a list of its datastreams
+1. Fetches each datastream's content file
+1. Generates a Bag containing all of the object's content files.
+
+To use it, edit the `$output_dir`, `$site_base_url`, `$namespace`, and `$limit` variables at the top of the fetch.php script, and then run:
+
+```
+php fetch.php
+```
+
+Your Bags will be in the directory specified in `$output_dir`.
 
 ## Maintainer
 
@@ -35,6 +42,11 @@ This script:
 ## Development and feedback
 
 Bug reports, use cases and suggestions are welcome. If you want to open a pull request, please open an issue first.
+
+## To do
+
+* Allow user to determine the specific Solr query that retrieves PIDs (currently the query retrieves all objects with a given namespace).
+* Allow user to populate bag-info.txt tags.
 
 ## License
 
--- fetch.php
+++ fetch.php
@@ -7,68 +7,67 @@
 // Where you want your Bags to be saved. Must exist.
 $output_dir = '/tmp/fetchbags';
 
-// The URL of your collection's first browse page.
-$browse_url = 'http://digital.lib.sfu.ca/hiv-collection/blair-henshaw-hiv-aids-stamp-collection';
-$browse_url_parts = parse_url($browse_url);
-$site_base_url = $browse_url_parts['scheme'] . '://' . $browse_url_parts['host'];
+// Your site's base URL.
+$site_base_url = 'http://digital.lib.sfu.ca';
 
-// This range corresponds to the number of pages in the collection's
-// browse list, the second number being the "?page" value of the last page.
-// @todo: automate getting this number.
-$pages = range(0, 6);
+// The namespace of the objects you want to generate Bags for.
+$namespace ='hiv';
+
+// Set to 1000000 or some ridiculously high number unless you want a subset.
+$limit = '10';
 
 /**
  * You will not need to change anything below this line.
  */
 
+
+// @todo: Allow user to define the Solr query.
+$solr_url = $site_base_url . '/islandora/rest/v1/solr/PID:' . $namespace . '\:*?fl=PID&rows=' . $limit;
+
 require_once 'vendor/autoload.php';
 require 'vendor/scholarslab/bagit/lib/bagit.php';
-use Goutte\Client;
 
-// Scrape each of the parameterized browse pages defined in $pages.
-$goutte_client = new Client();
-$object_urls = array();
-print "Scraping object URLs from collection browse pages starting at $browse_url.\n";
-foreach ($pages as $page) {
-    $crawler = $goutte_client->request('GET', $browse_url . '?page=' . $page);
-    $hrefs = $crawler->filter('dd.islandora-object-caption > a')->extract(array('href'));
-    $object_urls = array_merge($object_urls, $hrefs);
-}
+// Get the results of the Solr query.
+$client = new GuzzleHttp\Client();
+$solr_response = $client->request('GET', $solr_url, [
+       'headers' => [
+            'Accept' => 'application/json',
+            // 'X-Authorization-User' => $cmd['u'] . ':' . $cmd['t'],
+        ]
+]);
 
-$count = count($object_urls);
-print "Scrapted $count object URLs, starting to fetch content files.\n";
+$solr_response_body = $solr_response->getBody();
+$solr_results = json_decode($solr_response_body);
+$docs = $solr_results->response->docs;
 
-// Extract the PID from each object URL. This will be specific to the URLs on the site
-// e.g., specific to path auto URL patterns, etc.
-foreach ($object_urls as &$url) {
-    $url = ltrim($url, '/');
-    $pid = preg_replace('#/.*$#', '', $url);
-    $pid = preg_replace('#\-#', ':', $pid);
-    $describe_object_url = $site_base_url . '/islandora/rest/v1/object/' . $pid;
-    describe_object($describe_object_url, $pid);
+$count = count($docs);
+print "Retrieved $count object URLs, starting to fetch content files.\n";
+
+// Assemble each object URL and fetch datastream content.
+foreach ($docs as $doc) {
+    describe_object($doc->PID, $site_base_url);
 }
 
 /**
  * Gets the list of datastreams from Islandora's REST interface.
  *
- * @param string $url
- *   The "describe object" REST URL.
  * @param string $pid
  *   The object's PID.
+ * @param string $site_base_url
+ *   The site's base URL.
  */
-function describe_object($url, $pid) {
+function describe_object($pid, $site_base_url) {
+	$object_url = $site_base_url . '/islandora/rest/v1/object/' . $pid;
 	$client = new GuzzleHttp\Client();
-    $object_response = $client->request('GET', $url, [
+    $object_response = $client->request('GET', $object_url, [
        'headers' => [
             'Accept' => 'application/json',
             // 'X-Authorization-User' => $cmd['u'] . ':' . $cmd['t'],
         ]
     ]);
 
-    $object_response_body = $object_response->getBody();
-    $object_response_body_array = json_decode($object_response_body, true);
-    fetch_datastreams($object_response_body_array['datastreams'], $pid);
-
+    $object_response_body = json_decode($object_response->getBody());
+    fetch_datastreams($pid, $object_response_body->datastreams, $site_base_url);
 }
 
 /**
@@ -78,32 +77,80 @@ function describe_object($url, $pid) {
  *   The list of datastreams retrieved from the "describe object" URL.
  * @param string $pid
  *   The object's PID.
+ * @param string $site_base_url
+ *   The site's base URL.
  */
-function fetch_datastreams($datastreams, $pid) {
-	global $site_base_url;
+function fetch_datastreams($raw_pid, $datastreams, $site_base_url) {
 	global $output_dir;
-	$mimes = new \Mimey\MimeTypes;
-	$bag_output_dir = $output_dir . DIRECTORY_SEPARATOR . preg_replace('/:/', '_', $pid);
+
+    // Add some custom mimetype -> extension mappings.
+    $builder = \Mimey\MimeMappingBuilder::create();
+    $builder->add('text/xml', 'xml');
+
+    $mimes = new \Mimey\MimeTypes($builder->getMapping());
+
+    $pid = preg_replace('/:/', '_', $raw_pid);
+	$bag_output_dir = $output_dir . DIRECTORY_SEPARATOR . $pid;
 	@mkdir($bag_output_dir);
 	$client = new GuzzleHttp\Client();
+	$data_files = array();
     foreach ($datastreams as $ds) {
-        $ds_url = $site_base_url . '/islandora/rest/v1/object/' . $pid . '/datastream/' . $ds['dsid'];
+        $ds_url = $site_base_url . '/islandora/rest/v1/object/' . $raw_pid . '/datastream/' . $ds->dsid;
         $ds_response = $client->request('GET', $ds_url, [
             'headers' => [
                 // 'X-Authorization-User' => $cmd['u'] . ':' . $cmd['t'],
             ]
         ]);
 
-        $mimeTypes = $mimes->getAllExtensions($ds['mimeType']);
-        $file_path = $bag_output_dir . DIRECTORY_SEPARATOR . $ds['dsid'] . '.' . $mimeTypes[0];
+        $mimeTypes = $mimes->getAllExtensions($ds->mimeType);
+        $file_path = $bag_output_dir . DIRECTORY_SEPARATOR . $ds->dsid . '.' . $mimeTypes[0];
         file_put_contents($file_path, $ds_response->getBody());
+        $data_files[] = $file_path;
     }
-    print "Fetched content files for object $pid and saved them to temporary directory $bag_output_dir.\n";
+    generate_bag($pid, $bag_output_dir, $data_files);
 }
 
 /**
  * Generates a Bag from object's fetched content files.
+ *
+ * @param string $pid
+ *   The object's PID.
  */
-function generate_bag() {
-    // @todo: Generate Bag.
+function generate_bag($pid, $dir, $files) {
+	global $output_dir;
+	global $site_base_url;
+	// @todo: PIDs can contain _, so we need to fix this.
+	$pid_with_colon = preg_replace('/_/', ':', $pid);
+	$object_url = $site_base_url . '/islandora/object/' . $pid_with_colon;
+	$bag_info = array(
+	  'Internal-Sender-Identifier' => $object_url,
+	  'External-Description' => 'A simple Bag containing datastreams exported from ' . $pid_with_colon . '.',
+	  'Bagging-Date	' => date("Y-m-d"),
+	);
+
+	$bag = new BagIt($output_dir . DIRECTORY_SEPARATOR . $pid, true, true, true, $bag_info);
+	foreach ($files as $file) {
+	    $bag->addFile($file, basename($file));
+	}
+
+    $bag->update();
+    $bag->package($dir);
+    print "Bag for $object_url saved in $output_dir\n";
+
+    cleanup_temp_files($dir);
+}
+
+/**
+ * Deletes a directory and all its children.
+  *
+  * @param string $dir
+  *    The directory to delete.
+  */
+function cleanup_temp_files($dir) {
+	$files = array_diff(scandir($dir), array('.','..'));
+    foreach ($files as $file) {
+    	$child = $dir . DIRECTORY_SEPARATOR . $file;
+        is_dir($child) ? cleanup_temp_files($child) : unlink($child);
+    }
+    rmdir($dir);
 }
